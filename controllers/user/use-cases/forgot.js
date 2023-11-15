b91a77600f41be09391ecbe94d564978f063ec0e
yosriksonti <yosri.kossontini@esprit.tn>
2023-11-15 23:17:18 +0100

    remove link from frogot

--- controllers/user/use-cases/forgot.js
+++ controllers/user/use-cases/forgot.js
@@ -1,16 +1,13 @@
 
 module.exports = function makeForgot(db,E,utils){
     return async function forgot(req, res, next) {
-        let { email, callback } = req.body;
+        let { email } = req.body;
         let userExists = await db.User.findOne({email})
 
         const resetpassword = new db.ResetPassword({
                 email,
         });
         let response = await resetpassword.save();
-
-        let link = `${callback}${response.id}`;
-        response.link = link
 
         if (userExists) {
             // await utils.email.sendForgotPassword(link, email);
